d8f14b2077622a1877025404ef14f828246ab4b2
Figen YILMAZ <49810288+fgnyilmaz@users.noreply.github.com>
2022-01-31 15:23:09 +0100

    delete file

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('Hello everyone')
+
+
+import numpy as np
+
+r = '3mm'
+pi = '3.14'
+circ = 2*pi*r
+
+print(circ)
--- testPier.py
+++
@@ -1,7 +0,0 @@
-print("the file of Pier is amazing")
-
-def calc_area_square(l):
-    """
-    comment test pier
-    """
-    return l*l
